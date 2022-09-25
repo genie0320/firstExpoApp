41ded0de70ba81bc9ba2c357bbcaa73fb38cb015
genie0320 <genie0320@gmail.com>
2022-09-25 17:21:20 +0900

    ScrollView 02

--- firstExpoApp/App.js
+++ firstExpoApp/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-		//각 태그들에는 style이라는 속성을 갖습니다.
-		//이 속성은 파일 최하단에 작성한 스타일 코드 객체의 키 값을 부여해
-    // 엘리먼트들에 스타일을 줄 수 있습니다.
-    //이는 JSX문법을 배우고 난 다음 더 자세히 다룹니다.
-    <View style={styles.container}>
-			{/* //보인 영역을 갖는 엘리먼트 7가 반복적으로 쓰였습니다. */}
+    <ScrollView style={styles.container}>
       <View style={styles.textContainer}>
         <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
       </View>
@@ -30,13 +25,10 @@ export default function App() {
       <View style={styles.textContainer}>
         <Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
-//텍스트가 영역을 갖고, 가운데 정렬도 하며, 테두리 스타일을 갖게 끔 하는 코드입니다.
-//JSX를 마저 배우고 스타일에 대해 자세히 다루니
-//걱정 안해도 좋습니다!
 const styles = StyleSheet.create({
   container: {
     flex: 1,
